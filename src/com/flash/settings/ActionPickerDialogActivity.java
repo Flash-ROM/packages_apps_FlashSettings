kimjisoo/packages_apps_Tweaks

DUI: Initial N checkin
Change-Id: I98e639868eeb4c5d5496f73cd6c67345c7c2e57b

src/com/dirtyunicorns/dutweaks/ActionPickerDialogActivity.java
@@ -1,0 +1,170 @@
+/*
+ * Copyright (C) 2015-2016 DirtyUnicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Dialog allowing to pick a system action or a intent compatible with
+ * ActionHandler framework
+ */
+
+package com.dirtyunicorns.dutweaks;
+
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.settings.R;
+import com.dirtyunicorns.dutweaks.CustomActionListAdapter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+
+public class ActionPickerDialogActivity extends Activity implements
+        ShortcutPickHelper.OnPickListener {
+
+    private static final int DIALOG_ROOT = 1;
+    private static final int DIALOG_SYSTEM = 2;
+    private ShortcutPickHelper mPicker;
+    private CustomActionListAdapter mCustomActionListAdapter;
+    private boolean mHasDefault;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPicker = new ShortcutPickHelper(this, this);
+        mCustomActionListAdapter = new CustomActionListAdapter(this);
+        mHasDefault = getIntent().getBooleanExtra("has_defaults", false);
+        String[] excludedActions = getIntent().getStringArrayExtra("excluded_actions");
+        if (excludedActions != null) {
+            for (int i = 0; i < excludedActions.length; i ++) {
+                mCustomActionListAdapter.removeAction(excludedActions[i]);
+            }
+        }
+        createDialog(this, DIALOG_ROOT).show();
+    }
+
+    public Dialog createDialog(final Context context, final int id) {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final Dialog dialog;
+
+        final DialogInterface.OnClickListener l = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                switch (id) {
+                    case DIALOG_ROOT:
+                        onTargetChange(getResources().getStringArray(
+                                mHasDefault ? R.array.action_dialog_values
+                                        : R.array.action_dialog_no_default_values)[item]);
+                        break;
+                    case DIALOG_SYSTEM:
+                        sendResultAndFinish(mCustomActionListAdapter.getItem(item).getAction());
+                        finish();
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid dialog type "
+                                + id + " in ActionPicker dialog.");
+                }
+            }
+        };
+
+        final DialogInterface.OnCancelListener cancel = new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+                onTargetChange(null);
+            }
+        };
+
+        final DialogInterface.OnClickListener cancelClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                cancel.onCancel(dialog);
+            }
+        };
+
+        switch (id) {
+            case DIALOG_ROOT:
+                dialog = builder
+                        .setTitle(R.string.choose_action_title)
+                        .setItems(
+                                getResources().getStringArray(
+                                        mHasDefault ? R.array.action_dialog_entries
+                                                : R.array.action_dialog_no_default_entries), l)
+                        .setOnCancelListener(cancel)
+                        .setNegativeButton(getString(android.R.string.cancel), cancelClickListener)
+                        .create();
+                break;
+            case DIALOG_SYSTEM:
+                dialog = builder.setTitle(getString(R.string.action_entry_custom_action))
+                        .setAdapter(mCustomActionListAdapter, l)
+                        .setOnCancelListener(cancel)
+                        .setNegativeButton(getString(android.R.string.cancel), cancelClickListener)
+                        .create();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid dialog type "
+                        + id + " in ActionPicker dialog.");
+        }
+        return dialog;
+    }
+
+    private void sendResultAndFinish(String result) {
+        Intent intent = new Intent("intent_action_action_picker");
+        intent.putExtra("result", Activity.RESULT_OK);
+        intent.putExtra("action_string", result);
+        ActionConfig actionConfig = new ActionConfig(this, result);
+        intent.putExtra("action_config", actionConfig);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+    private void sendCancelResultAndFinish() {
+        Intent intent = new Intent("intent_action_action_picker");
+        intent.putExtra("result", Activity.RESULT_CANCELED);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+    @Override
+    public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
+        if (uri == null) {
+            sendCancelResultAndFinish();
+        } else {
+            sendResultAndFinish(uri);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPicker.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void onTargetChange(String uri) {
+        if (uri == null) {
+            sendCancelResultAndFinish();
+        } else if (uri.equals(getString(R.string.action_value_default_action))) {
+            sendResultAndFinish("default");
+        } else if (uri.equals(getString(R.string.action_value_select_app))) {
+            mPicker.pickShortcut(null, null, 0);
+        } else if (uri.equals(getString(R.string.action_value_custom_action))) {
+            createDialog(this, DIALOG_SYSTEM).show();
+        }
+    }
+}

src/com/dirtyunicorns/dutweaks/CustomActionListAdapter.java
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.dirtyunicorns.dutweaks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.utils.du.ActionHandler;
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}

Flash-ROM/packages_apps_FlashSettings

src/com/flash/settings/ActionPickerDialogActivity.java
@@ -1,0 +1,170 @@
+/*
+ * Copyright (C) 2015-2016 DirtyUnicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Dialog allowing to pick a system action or a intent compatible with
+ * ActionHandler framework
+ */
+
+package com.flash.settings;
+
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.settings.R;
+import com.flash.settings.CustomActionListAdapter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserHandle;
+
+public class ActionPickerDialogActivity extends Activity implements
+        ShortcutPickHelper.OnPickListener {
+
+    private static final int DIALOG_ROOT = 1;
+    private static final int DIALOG_SYSTEM = 2;
+    private ShortcutPickHelper mPicker;
+    private CustomActionListAdapter mCustomActionListAdapter;
+    private boolean mHasDefault;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPicker = new ShortcutPickHelper(this, this);
+        mCustomActionListAdapter = new CustomActionListAdapter(this);
+        mHasDefault = getIntent().getBooleanExtra("has_defaults", false);
+        String[] excludedActions = getIntent().getStringArrayExtra("excluded_actions");
+        if (excludedActions != null) {
+            for (int i = 0; i < excludedActions.length; i ++) {
+                mCustomActionListAdapter.removeAction(excludedActions[i]);
+            }
+        }
+        createDialog(this, DIALOG_ROOT).show();
+    }
+
+    public Dialog createDialog(final Context context, final int id) {
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final Dialog dialog;
+
+        final DialogInterface.OnClickListener l = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                switch (id) {
+                    case DIALOG_ROOT:
+                        onTargetChange(getResources().getStringArray(
+                                mHasDefault ? R.array.action_dialog_values
+                                        : R.array.action_dialog_no_default_values)[item]);
+                        break;
+                    case DIALOG_SYSTEM:
+                        sendResultAndFinish(mCustomActionListAdapter.getItem(item).getAction());
+                        finish();
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid dialog type "
+                                + id + " in ActionPicker dialog.");
+                }
+            }
+        };
+
+        final DialogInterface.OnCancelListener cancel = new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+                onTargetChange(null);
+            }
+        };
+
+        final DialogInterface.OnClickListener cancelClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                cancel.onCancel(dialog);
+            }
+        };
+
+        switch (id) {
+            case DIALOG_ROOT:
+                dialog = builder
+                        .setTitle(R.string.choose_action_title)
+                        .setItems(
+                                getResources().getStringArray(
+                                        mHasDefault ? R.array.action_dialog_entries
+                                                : R.array.action_dialog_no_default_entries), l)
+                        .setOnCancelListener(cancel)
+                        .setNegativeButton(getString(android.R.string.cancel), cancelClickListener)
+                        .create();
+                break;
+            case DIALOG_SYSTEM:
+                dialog = builder.setTitle(getString(R.string.action_entry_custom_action))
+                        .setAdapter(mCustomActionListAdapter, l)
+                        .setOnCancelListener(cancel)
+                        .setNegativeButton(getString(android.R.string.cancel), cancelClickListener)
+                        .create();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid dialog type "
+                        + id + " in ActionPicker dialog.");
+        }
+        return dialog;
+    }
+
+    private void sendResultAndFinish(String result) {
+        Intent intent = new Intent("intent_action_action_picker");
+        intent.putExtra("result", Activity.RESULT_OK);
+        intent.putExtra("action_string", result);
+        ActionConfig actionConfig = new ActionConfig(this, result);
+        intent.putExtra("action_config", actionConfig);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+    private void sendCancelResultAndFinish() {
+        Intent intent = new Intent("intent_action_action_picker");
+        intent.putExtra("result", Activity.RESULT_CANCELED);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+    @Override
+    public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
+        if (uri == null) {
+            sendCancelResultAndFinish();
+        } else {
+            sendResultAndFinish(uri);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mPicker.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void onTargetChange(String uri) {
+        if (uri == null) {
+            sendCancelResultAndFinish();
+        } else if (uri.equals(getString(R.string.action_value_default_action))) {
+            sendResultAndFinish("default");
+        } else if (uri.equals(getString(R.string.action_value_select_app))) {
+            mPicker.pickShortcut(null, null, 0);
+        } else if (uri.equals(getString(R.string.action_value_custom_action))) {
+            createDialog(this, DIALOG_SYSTEM).show();
+        }
+    }
+}

src/com/flash/settings/CustomActionListAdapter.java
@@ -1,0 +1,122 @@
+/*
+ * Copyright (C) 2015 TeamEos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Adapter for displaying custom actions in a list
+ */
+
+package com.flash.settings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.android.internal.utils.du.ActionHandler;
+import com.android.internal.utils.du.Config.ActionConfig;
+import com.android.settings.R;
+
+public class CustomActionListAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<ActionConfig> mCustomActions = new ArrayList<ActionConfig>();
+
+    public CustomActionListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        reloadActions();
+    }
+
+    private void reloadActions() {
+        mCustomActions.clear();
+        mCustomActions.addAll(ActionHandler.getSystemActions(mContext));
+        notifyDataSetChanged();
+    }
+
+    public void removeAction(String action) {
+        int index = -1;
+        for (int i = 0; i < mCustomActions.size(); i++) {
+            if (TextUtils.equals(mCustomActions.get(i).getAction(), action)) {
+                index = i;
+                break;
+            }
+        }
+        if (index > -1) {
+            mCustomActions.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mCustomActions.size();
+    }
+
+    @Override
+    public ActionConfig getItem(int position) {
+        return mCustomActions.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Context ctx = mContext;
+        if (convertView != null) {
+            holder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = mInflater.inflate(R.layout.custom_action_item, null, false);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
+            holder.title = (TextView) convertView.findViewById(com.android.internal.R.id.title);
+            holder.summary = (TextView) convertView
+                    .findViewById(com.android.internal.R.id.summary);
+            holder.icon = (ImageView) convertView.findViewById(R.id.icon);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) holder.icon.getLayoutParams();
+            params.width = holder.icon.getMaxWidth();
+            params.height = holder.icon.getMaxHeight();
+            holder.icon.setLayoutParams(params);
+            holder.icon.setScaleType(ScaleType.CENTER);
+            holder.icon.setCropToPadding(true);
+        }
+
+        ActionConfig config = getItem(position);
+        holder.title.setText(config.getLabel());
+        holder.icon.setBackgroundResource(R.drawable.fab_accent);
+        holder.icon.setImageDrawable(config.getDefaultIcon(ctx));
+        holder.summary.setVisibility(View.GONE);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+        TextView title;
+        TextView summary;
+        ImageView icon;
+    }
+}